d3e8abe5f2e3752d879b7a5b615289bd9e97b97f
Yiwei Liu <2814824250@qq.com>
2023-11-24 14:58:30 +0000

    Mongo lab Error Handling

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -1,15 +1,20 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-  title:  String,
-  description:  String ,
+  title: { type: String, required: true },
+  description: String,
   deadline: Date,
-    done: Boolean,
-    priority: {type: String, enum: ["Low","Medium","High"]},
-    created_at: Date,
-    updated_at: Date
+  done: Boolean,
+  priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
+  created_at: Date,
+  updated_at: Date,
 });
 
-export default mongoose.model('Task', TaskSchema);
+const dateValidator = (date) => {
+  return date > new Date();
+};
+TaskSchema.path("deadline").validate(dateValidator);
+
+export default mongoose.model("Task", TaskSchema);
